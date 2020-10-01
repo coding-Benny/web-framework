49a0aa96ef0d95c981015b9bc8b3da4a9dc4d831
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-10-01 01:43:54 -0700

    🐶 Annotation-based Configuration 🐱

--- helloDI/src/main/java/kr/ac/hansung/cse/PetOwner.java
+++ helloDI/src/main/java/kr/ac/hansung/cse/PetOwner.java
@@ -1,9 +1,12 @@
 package kr.ac.hansung.cse;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
-@AllArgsConstructor
 public class PetOwner {
+	
+	@Autowired	// wiring by type
+	@Qualifier(value="qf_cat")
 	private AnimalType animal;
 	
 	public void play() {
