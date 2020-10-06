2458068550f9fc4bf7657f5afeb0560a13a363a2
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-10-06 11:56:08 +0900

    🐕 @AspectJ based AOP 🐈

--- helloAOP/src/main/java/kr/ac/hansung/cse/Logger.java
+++ helloAOP/src/main/java/kr/ac/hansung/cse/Logger.java
@@ -1,7 +1,17 @@
 package kr.ac.hansung.cse;
 
-public class Logger {
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
+@Aspect
+public class Logger {
+	@Pointcut("execution(void kr.ac.hansung.cse.*.sound())")
+	private void selectSound() {
+		
+	}
+	
+	@Before("selectSound()")
 	public void aboutToSound() {
 		System.out.println("advicd: about to sound");
 	}
