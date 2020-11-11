f16727455734a42c208911d8cecef2a158979ea1
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-11-11 14:09:24 +0900

    📤 Spring data buffering

--- helloSpringMVC/src/main/java/kr/ac/hansung/controller/OfferController.java
+++ helloSpringMVC/src/main/java/kr/ac/hansung/controller/OfferController.java
@@ -2,9 +2,13 @@ package kr.ac.hansung.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.ac.hansung.model.Offer;
@@ -32,9 +36,19 @@ public class OfferController {
 	}
 	
 	@RequestMapping("/docreate")
-	public String doCreate(Model model, Offer offer) {
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
 		
-		// System.out.println(offer);	Controller -> Service -> Dao
+		if (result.hasErrors()) {
+			System.out.println("== Form data does not validated ==");
+			
+			List<ObjectError> errors = result.getAllErrors();
+			
+			for (ObjectError error : errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+			
+			return "createoffer";
+		}
 		
 		offerService.insert(offer);
 		
