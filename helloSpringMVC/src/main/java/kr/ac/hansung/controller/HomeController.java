b9749b7c93401a9fba43dd177aae7f021cdc9920
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-12-05 15:26:59 +0900

    📝 Logback

--- helloSpringMVC/src/main/java/kr/ac/hansung/controller/HomeController.java
+++ helloSpringMVC/src/main/java/kr/ac/hansung/controller/HomeController.java
@@ -1,11 +1,13 @@
 package kr.ac.hansung.controller;
 
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  * Handles requests for the application home page.
@@ -13,8 +15,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	//private static final Logger LOGGER = LoggerFactory.getLogger("kr.ac.hansung.controller.HomeController");
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(HttpServletRequest request) {
+		
+		String url = request.getRequestURL().toString();
+		String clientIPaddr = request.getRemoteAddr();
+		
+		LOGGER.info("Request URL: {}, Client IP: {}", url, clientIPaddr);
 		
 		return "home";
 	}
