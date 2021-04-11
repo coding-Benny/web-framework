c1bfb54fc5869ebff341c60fb3ed027651c50bb2
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-04-11 10:33:22 +0900

    🤹 Handling exceptions

--- helloRest/src/main/java/kr/ac/hansung/cse/controller/RestApiController.java
+++ helloRest/src/main/java/kr/ac/hansung/cse/controller/RestApiController.java
@@ -2,11 +2,14 @@ package kr.ac.hansung.cse.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,28 +19,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import kr.ac.hansung.cse.exception.ErrorResponse;
+import kr.ac.hansung.cse.exception.UserDuplicatedException;
+import kr.ac.hansung.cse.exception.UserNotFoundException;
 import kr.ac.hansung.cse.model.User;
 import kr.ac.hansung.cse.service.UserService;
 
-@RestController	// @Controller + @ResponseBody
+@RestController // @Controller + @ResponseBody
 @RequestMapping("/api")
 public class RestApiController {
 	@Autowired
 	UserService userService;
-	
+
 	// --- Retrieve All Users
 	// @RequestMapping(value = "/users", method = RequestMethod.GET)
-	@GetMapping(value = "/users")	// http://localhost:8080/helloRest/api/users
+	@GetMapping(value = "/users") // http://localhost:8080/helloRest/api/users
 	public ResponseEntity<List<User>> listAllUsers() {
 		List<User> users = userService.findAllUsers();
-		
+
 		if (users.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);	// status
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT); // status
 		}
-		
-		return new ResponseEntity<List<User>>(users, HttpStatus.OK);	// body(json), status
+
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK); // body(json), status
 	}
-	
+
 	// --- Retrieve Single User
 	// @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
 	@GetMapping(value = "/users/{id}")
@@ -49,9 +55,10 @@ public class RestApiController {
 		}
 		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
-	
+
 	// --- Create a User
-	// @RequestMapping(value = "/users", method = RequestMethod.POST)	// request body(json)
+	// @RequestMapping(value = "/users", method = RequestMethod.POST) // request
+	// body(json)
 	@PostMapping(value = "/users")
 	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder ucBuilder) {
 		if (userService.doesUserExist(user)) {
@@ -59,32 +66,32 @@ public class RestApiController {
 			throw new UserDuplicatedException(user.getName());
 		}
 		userService.saveUser(user);
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setLocation(ucBuilder.path("/api/users/{id}").buildAndExpand(user.getId()).toUri());
-		return new ResponseEntity<>(headers, HttpStatus.CREATED);	// status, headers
+		return new ResponseEntity<>(headers, HttpStatus.CREATED); // status, headers
 	}
-	
+
 	// --- Update a User
 	// @RequestMapping(value = "/users/{id}", method = RequestMethod.PUT)
 	@PutMapping(value = "/users/{id}")
 	public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
 		User currentUser = userService.findById(id);
-		
+
 		if (currentUser == null) {
 			// to do list: exception
 			throw new UserNotFoundException(id);
 		}
-		
+
 		currentUser.setName(user.getName());
 		currentUser.setAge(user.getAge());
 		currentUser.setSalary(user.getSalary());
-		
+
 		userService.updateUser(currentUser);
-		
+
 		return new ResponseEntity<User>(currentUser, HttpStatus.OK);
 	}
-	
+
 	// --- Delete a User
 	// @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
 	@DeleteMapping(value = "/users/{id}")
@@ -97,7 +104,7 @@ public class RestApiController {
 		userService.deleteUserById(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
-	
+
 	// --- Delete All Users
 	// @RequestMaping(value = "/users", method = RequestMethod.DELETE)
 	@DeleteMapping(value = "/users")
