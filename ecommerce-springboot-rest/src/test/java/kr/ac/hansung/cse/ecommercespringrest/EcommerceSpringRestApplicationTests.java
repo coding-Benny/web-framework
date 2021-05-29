4c32ab33874c48145fee49bb457f0f7a685d8b17
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-05-29 11:02:38 +0900

    👤 Add Charlies' acct. using BCryptPasswordEncoder

--- ecommerce-springboot-rest/src/test/java/kr/ac/hansung/cse/ecommercespringrest/EcommerceSpringRestApplicationTests.java
+++ ecommerce-springboot-rest/src/test/java/kr/ac/hansung/cse/ecommercespringrest/EcommerceSpringRestApplicationTests.java
@@ -1,10 +1,21 @@
 package kr.ac.hansung.cse.ecommercespringrest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class EcommerceSpringRestApplicationTests {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Test
+    void generatedHashedPassword() {
+        String pwd = encoder.encode("charliepw");
+        System.out.println(pwd);
+    }
 
     @Test
     void contextLoads() {
