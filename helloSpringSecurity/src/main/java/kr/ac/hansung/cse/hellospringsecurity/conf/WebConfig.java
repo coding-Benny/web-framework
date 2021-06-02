2a2cbe375d9355857d6fc2272090d4d2f6e6b7bc
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-06-02 11:44:30 +0900

    🔐 Spring security tutorial

---
+++ helloSpringSecurity/src/main/java/kr/ac/hansung/cse/hellospringsecurity/conf/WebConfig.java
@@ -0,0 +1,22 @@
+package kr.ac.hansung.cse.hellospringsecurity.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
+import javax.swing.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
+}
