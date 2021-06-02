7c05ae4bf3920c5baa4fbb3c04ea7217c7f4d7f3
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-06-02 15:47:18 +0900

    🛠 Fix autowired error

--- helloSpringSecurity/src/main/java/kr/ac/hansung/cse/hellospringsecurity/conf/WebSecurityConfig.java
+++ helloSpringSecurity/src/main/java/kr/ac/hansung/cse/hellospringsecurity/conf/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -19,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private UserDetailsService customUserDetailsService;
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
