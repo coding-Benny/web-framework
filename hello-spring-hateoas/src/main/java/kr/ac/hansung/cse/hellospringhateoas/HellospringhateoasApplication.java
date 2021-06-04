d23e30941ba5863a564b03eff2a78326e6f99387
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-06-04 11:55:32 +0900

    ⚙ Setup Spring HATEOAS

---
+++ hello-spring-hateoas/src/main/java/kr/ac/hansung/cse/hellospringhateoas/HellospringhateoasApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.hellospringhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringhateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringhateoasApplication.class, args);
+    }
+
+}
